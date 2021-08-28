273f9a62a6e505cdd9df65b18b506489c7251004
Kaushalendra Pandey <pandeysid713@gmail.com>
2021-08-29 01:51:40 +0530

    bug: added alert if text is empty or api is failing to return the result

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 inputText = document.getElementById("input-text")
 button = document.getElementById("button")
 let translatedBox = document.getElementById("translated-text")
+alertBox = document.getElementsByClassName("alert")
 
 const getEnteredText = async () => {
     const entered_text = inputText.value
+    alertBox[0].innerHTML = ""
+    translatedBox.innerHTML = ""
+    if(entered_text.length === 0){
+        alertBox[0].innerHTML = "Please enter something in the box"
+        return
+    }
     let translatedText = await getTranslatedText(entered_text)
     addToTranslatedBox(translatedText)
 }
@@ -19,6 +26,9 @@ const getTranslatedText = async (entered_text) => {
 const addToTranslatedBox = (translatedText) => {
     translatedBox.innerHTML = ""    
     para = document.createElement("p")
+    if(translatedText === undefined){
+        translatedText = "Please try again after some time.. You might have hit the api so many time."
+    }
     para.innerHTML = translatedText
     translatedBox.appendChild(para)
 }
